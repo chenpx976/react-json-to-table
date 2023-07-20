b9f6a17ed04ca92db1206ec7c3724d4f875a2636
chenpx976 <chenpx976@gmail.com>
2023-07-20 10:14:46 +0800

    feat: 优化构建

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'father';
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
   esm: { output: 'dist' },
-  umd: { output: 'dist' },
+  umd: {
+    output: 'dist/umd',
+    extractCSS: false,
+    externals: { react: 'React' },
+    entry: 'src/index',
+  },
 });
